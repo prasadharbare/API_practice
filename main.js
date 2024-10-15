1e070bc2154974722838090876e73274327f3667
prasadharbare <prasad1523@proton.me>
2024-10-15 04:12:22 +0000

    1st section done

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-const img1=document.querySelector("img")
-async function getData() {
-    const res = await fetch("https://dog.ceo/api/breed//images/random");
-    const data = await res.json
+// https://dog.ceo/api/breed/affenpinscher/images/random
+// https://dog.ceo/api/breeds/list/all
+
+const imageEl = document.querySelector("img");
+
+async function getImage() {
+  const res = await fetch(
+    `https://dog.ceo/api/breed/affenpinscher/images/random`
+  );
+  const data = await res.json();
+  console.log(data.message);
+
+  imageEl.src = data.message;
 }
+
+getImage();
